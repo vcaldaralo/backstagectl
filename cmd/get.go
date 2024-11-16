1a357447c4770b0b5a002eb20deb1233a3eb753e
vcaldaralo <vito.caldaralo@sysdig.com>
2024-11-16 16:35:38 +0100

    fix flag releation in get cmd

--- cmd/get.go
+++ cmd/get.go
@@ -18,15 +18,19 @@ var getEntityCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		initAuth() // Initialize authentication
 
-		ref, _ := cmd.Flags().GetString("ref")             // Get the ref flag
-		kind, _ := cmd.Flags().GetString("kind")           // Get the kind flag
-		name, _ := cmd.Flags().GetString("name")           // Get the name flag
-		namespace, _ := cmd.Flags().GetString("namespace") // Get the namespace flag
-		filterKeys, _ := cmd.Flags().GetBool("filter")     // Get the filter flag
+		ref, _ := cmd.Flags().GetString("ref")                 // Get the ref flag
+		kind, _ := cmd.Flags().GetString("kind")               // Get the kind flag
+		name, _ := cmd.Flags().GetString("name")               // Get the name flag
+		namespace, _ := cmd.Flags().GetString("namespace")     // Get the namespace flag
+		ancestry, _ := cmd.Flags().GetBool("ancestry")         // Get the filter flag
+		filterRelations, _ := cmd.Flags().GetBool("relations") // Get the filter flag
+
+		if len(kind) > 0 && kind[len(kind)-1] == 's' {
+			kind = kind[:len(kind)-1] // Remove the last character
+		}
 
 		// Split ref into kind, namespace, and name
 		if ref != "" {
-			fmt.Printf("%v\n", ref)
 			tokens := strings.Split(ref, ":") // Split by ':'
 			if len(tokens) == 2 {
 				kind = tokens[0]                   // First token is kind
@@ -39,7 +43,10 @@ var getEntityCmd = &cobra.Command{
 				}
 			}
 		}
-		url := fmt.Sprintf("%s/api/catalog/entities/by-name/%s/%s/%s", baseUrl, kind, namespace, name) // Use ref for URL
+
+		anc := map[bool]string{false: "", true: "/ancestry"}[ancestry]
+
+		url := fmt.Sprintf("%s/api/catalog/entities/by-name/%s/%s/%s%s", baseUrl, kind, namespace, name, anc) // Use ref for URL
 
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
@@ -73,7 +80,7 @@ var getEntityCmd = &cobra.Command{
 				return
 			}
 
-			if filterKeys {
+			if !filterRelations {
 				// Keys to filter (exclude these keys)
 				excludeKeys := map[string]bool{"relations": true}
 
@@ -108,11 +115,12 @@ var getEntityCmd = &cobra.Command{
 }
 
 func init() {
-	getEntityCmd.Flags().StringP("ref", "r", "", "The reference of the entity")              // Add kind flag
-	getEntityCmd.Flags().StringP("kind", "k", "", "The kind of the entity")                  // Add kind flag
-	getEntityCmd.Flags().StringP("name", "n", "", "The name of the entity")                  // Add name flag
-	getEntityCmd.Flags().StringP("namespace", "N", "default", "The namespace of the entity") // Add namespace flag
-	getEntityCmd.Flags().BoolP("filter", "f", true, "Neat specific keys from the output")    // Add filter flag
+	getEntityCmd.Flags().StringP("ref", "r", "", "The reference of the entity {kind}:[default/]{name} (ie. component:default/zookeeper)") // Add kind flag
+	getEntityCmd.Flags().StringP("kind", "k", "", "The kind of the entity [Resource|Component|System|Domain|User|Group|Location]")        // Add kind flag
+	getEntityCmd.Flags().StringP("name", "n", "", "The name of the entity")                                                               // Add name flag
+	getEntityCmd.Flags().StringP("namespace", "N", "default", "The namespace of the entity")                                              // Add namespace flag
+	getEntityCmd.Flags().BoolP("ancestry", "a", false, "Wheter or not tho retrieve the ancestry of the entity")                           // Add name flag
+	getEntityCmd.Flags().BoolP("relations", "l", false, "Print all relations")                                                            // Add filter flag
 	// Mark name as required
 	rootCmd.AddCommand(getEntityCmd) // Add the new command to the list command
 }
